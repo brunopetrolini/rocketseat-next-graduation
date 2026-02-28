2b48f906036855321b57a02c99d8769d72eb930b
Bruno Petrolini <bruno.petrolini@outlook.com.br>
2026-02-28 10:04:45 -0300

    feat(blog): refactor blog page structure and integrate search input

--- site-blog/src/pages/blog/index.tsx
+++ site-blog/src/pages/blog/index.tsx
@@ -1,26 +1,5 @@
-import { SearchInput } from '@/components/ui/search-input';
+import { BlogPage } from '@/templates/blog';
 
-export default function BlogPage() {
-  return (
-    <div className="container mt-24 flex h-full grow flex-col">
-      <header className="flex flex-col gap-6 md:flex-row md:justify-between">
-        <div className="flex flex-col gap-3 md:max-w-md">
-          {/* Tag */}
-          <span className="w-fit rounded-sm bg-cyan-300 px-3 py-1.5 text-body-tag text-cyan-100">
-            Blog
-          </span>
-
-          {/* Title */}
-          <h1 className="text-left font-sans text-gray-100 text-heading-lg md:text-heading-xl">
-            Dicas e estratégias para impulsionar seu negócio
-          </h1>
-        </div>
-
-        {/* Search */}
-        <SearchInput />
-      </header>
-
-      {/* Posts */}
-    </div>
-  );
+export default function Blog() {
+  return <BlogPage />;
 }
